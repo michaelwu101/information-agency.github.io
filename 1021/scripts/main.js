a4a2af21de61aebb458a838eba72de8452dd4499
12 207吳翊華 <michaelwu239@gmail.com>
2022-12-09 15:33:34 +0800

    12/9

--- 1021/scripts/main.js
+++ 1021/scripts/main.js
@@ -1,2 +1,9 @@
 let myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+function toggle_info() {
+  const foo = document.getElementById("foo");
+  // 取得 `foo` 的 class 列表。
+  // `toggle` 會賦予元素給定的 class，或是移除給定的 class，
+  // 取決於 class 是否已經存在
+  foo.classList.toggle("invisible");
+}
