06191121a17c951fd94b473ef8593b7f5409eb40
12 207吳翊華 <michaelwu239@gmail.com>
2022-12-09 16:08:57 +0800

    12/19

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+function toggle_info() {
+  const foo = document.getElementById("cat1");
+  // 取得 `foo` 的 class 列表。
+  // `toggle` 會賦予元素給定的 class，或是移除給定的 class，
+  // 取決於 class 是否已經存在
+  foo.classList.toggle("invisible");
+}
+function toggle_info() {
+  const foo = document.getElementById("cat2");
+  // 取得 `foo` 的 class 列表。
+  // `toggle` 會賦予元素給定的 class，或是移除給定的 class，
+  // 取決於 class 是否已經存在
+  foo.classList.toggle("invisible");
+}
+function toggle_info() {
+  const foo = document.getElementById("cat3");
+  // 取得 `foo` 的 class 列表。
+  // `toggle` 會賦予元素給定的 class，或是移除給定的 class，
+  // 取決於 class 是否已經存在
+  foo.classList.toggle("invisible");
+}
